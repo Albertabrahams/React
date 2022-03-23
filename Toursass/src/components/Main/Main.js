6fab8df11c14094f0ded06319d257601fc3afa88
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-03-23 23:30:28 +0300

    project done

--- Toursass/src/components/Main/Main.js
+++ Toursass/src/components/Main/Main.js
@@ -1,4 +1,3 @@
-
 import "./Main.scss";
 import { data } from "../../helper/data";
 import Card from "./Card";
@@ -7,7 +6,9 @@ const Main = () => {
     console.log(data);
     return(
         <div className="card-container">
-            {data.map((item, index) => ( <Card {...item} key={index}/> ))}
+            {data.map((item, index) => ( <Card {...item} key={index}/> 
+            
+            ))}
         </div>
     )
 }
